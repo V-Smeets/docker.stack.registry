05562087afa9c75dfe5f1171e7d845b639f076fd
Vincent Smeets <Vincent.Smeets@atos.net>
2020-04-30 09:46:44 +0200

    Use the correct digist to delete an image

--- delete-repoitory-tag.sh
+++ delete-repoitory-tag.sh
@@ -3,49 +3,40 @@
 
 registryUrl="http://127.0.0.1:5000"
 
-function getRequest() {
-	local path="$1"
+function getDigest() {
+	local repository="${1:?No repository defined}"
+	local tag="${2:?No tag defined}"
 
+	local heaerFile=$(mktemp)
 	curl \
 		--silent \
 		--header "Accept: application/vnd.docker.distribution.manifest.v2+json" \
-		"${registryUrl}${path}"
+		--dump-header "$heaerFile" \
+		"${registryUrl}/v2/${repository}/manifests/${tag}" \
+		>/dev/null
+	awk '$1 == "Docker-Content-Digest:" { print $2 }' "$heaerFile" \
+	| tr --complement --delete '[:alnum:][:punct:]'
+	rm -f "$heaerFile"
 }
 
-function deleteRequest() {
-	local path="$1"
+function deleteDigest() {
+	local repository="${1:?No repository defined}"
+	local digest="${2:?No digest defined}"
 
 	curl \
 		--silent \
 		--request DELETE \
-		--header "Accept: application/vnd.docker.distribution.manifest.v2+json" \
-		"${registryUrl}${path}"
-}
-
-function deleteDigest() {
-	local repository="$1"
-	local digest="$2"
-
-	deleteRequest "/v2/${repository}/manifests/${digest}"
-}
-
-function getManifest() {
-	local repository="$1"
-	local tag="$2"
-
-	getRequest "/v2/${repository}/manifests/${tag}"
-}
-
-function getDigest() {
-	local repository="$1"
-	local tag="$2"
-
-	getManifest "$repository" "$tag" \
-	| jq --raw-output '.config.digest'
+		"${registryUrl}/v2/${repository}/manifests/${digest}"
 }
 
 repository="${1:?No repository defined}"
 tag="${2:?No tag defined}"
 
 digest=$(getDigest "$repository" "$tag")
+if [[ -z "$digest" ]]
+then
+	echo "${repository}:${tag}" "does not exists"
+	exit 1
+fi
+
 deleteDigest "$repository" "$digest" | jq
